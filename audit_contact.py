f55be7bce5e1b255d1cfa7bc5c54bca47d5e73ed
Daniel Teobaldo <daniel.teobaldo@dtidigital.com.br>
2017-10-11 10:40:41 -0300

    Feita a correção de numeros de telefone que finalizavam  com 8 numeros em sequencia para o formato +55 31 XXXX-XXXX

--- audit_contact.py
+++ audit_contact.py
@@ -4,34 +4,62 @@ from collections import defaultdict
 
 OSM_FILE = "bh_map.osm"
 
-phonenumber_re = re.compile(r'\+55 31 [0-9]{4}.?[0-9]{4}')
+valid_contact_tag = ["contact:phone","phone","contact:email","email","contact:website","website"]
+
+phonenumber_re = re.compile(r'\+?55 31 [0-9]{4,5}.[0-9]{4}')
+phonenumber_onlynumber_re = re.compile(r'\+?[5]{0,2} ?(31)? ?[0-9]{8}')
 email_re = re.compile(r'^\S+@\S+\.\w+\.?\w?')
 site_re = re.compile(r'w{3}')
 
 invalid_phone_number_list = []
 invalid_email_list = []
 
-def audit_phone_number(phonenumber):
+def convert_phone_number_to_list(phonenumber):
     """ 
-    Remove double spaces and parentesis and
-    check if the phone number match the format +55 31 XXXX-XXXX ou +55 31 XXXXXXXX.
     If there is a number list separeted by semicolon, return an array with the number in list
     """
+    if phonenumber.find(";") > 0:
+        return phonenumber.split(";")
+    else:
+        return [phonenumber]
+
+def clean_phone_number(phonenumber):
+    """
+    Remove double spaces and parentesis and
+    check if the phone number match the format +55 31 [X]XXXX-XXXX ou +55 31 [X]XXXX XXXX.
+    """
+    phonenumber = phonenumber.strip()
     phonenumber = phonenumber.replace("  "," ")
-    phonenumber = phonenumber.replace("("," ")
-    phonenumber = phonenumber.replace(")"," ")
+    phonenumber = phonenumber.replace("(","")
+    phonenumber = phonenumber.replace(")","")
+
+    if phonenumber_onlynumber_re.match(phonenumber):
+        phonenumber = "+55 31 "+phonenumber[-8:-4]+"-"+phonenumber[-4:]
 
     if phonenumber_re.fullmatch(phonenumber):
-        return [phonenumber]
-    elif phonenumber.find(";") > 0:
-         phone_list = phonenumber.split(";")
-         for phone in phone_list:
-            if phonenumber_re.fullmatch(phone) == None:
-                invalid_email_list.append(phonenumber)
-            else:
-                return phone_list
+        if not phonenumber.startswith("+"):
+            phonenumber = "+"+phonenumber
+        return phonenumber
     else:
-        invalid_phone_number_list.append(phonenumber)
+        return None
+
+def audit_phone_number(phonenumber):
+    """
+    Log invalid phone numbers to be evaluated
+    """
+    phone_list = convert_phone_number_to_list(phonenumber)
+
+    returned_phone_list = []
+
+    for phone in phone_list:
+        cleaned_phone = clean_phone_number(phone)
+        if (cleaned_phone):
+            returned_phone_list.append(cleaned_phone)
+        else:
+            invalid_phone_number_list.append(phone)
+
+    return returned_phone_list
+
 
 def audit_email(email):
     """
@@ -49,13 +77,13 @@ def audit_website(site):
         print(site)
 
 def is_phone_element(element):
-    return element.tag == "tag" and element.attrib['k'] == "contact:phone"
+    return element.tag == "tag" and (element.attrib['k'] == "contact:phone" or element.attrib['k'] == "phone")
 
 def is_email_element(element):
-    return element.tag == "tag" and element.attrib['k'] == "contact:email"
+    return element.tag == "tag" and (element.attrib['k'] == "contact:email" or element.attrib['k'] == "email")
 
 def is_site_element(element):
-    return element.tag == "tag" and element.attrib['k'] == "contact:website"
+    return element.tag == "tag" and (element.attrib['k'] == "contact:website" or element.attrib['k'] == "website")
 
 def audit_contact(filename):
     """ Check the address data from OSM file """
