1345f14d8e7f3b5fcdf8cbba9b77549d32c84171
Daniel Teobaldo <daniel.teobaldo@dtidigital.com.br>
2017-10-13 14:22:54 -0300

    Ajustes na conversão do OSM para JSON.  Foram criados arquivos separados para as estrutras de Node, Way e Relation.

--- convert_osm_json.py
+++ convert_osm_json.py
@@ -3,35 +3,20 @@ Prepare the OSM file from Belo Horizonte to a JSON format to be imported to Mong
 """
 import xml.etree.cElementTree as ET
 import pprint
-import time
 import json
 import codecs
 import audit_address
 import audit_contact
 
-
+#Files
 OSM_FILE = "bh_map.osm"
-CREATED_TAGS = ["version", "changeset", "timestamp", "user", "uid"]
-KEYS_TO_CONVERT = ["amenity", "name"]
-JSON_DATA = []
+JSON_NODE_OUT = codecs.open("bh_node.json", "w", "utf-8")
+JSON_WAY_OUT = codecs.open("bh_way.json", "w", "utf-8")
+JSON_RELATION_OUT = codecs.open("bh_relation.json", "w", "utf-8")
 
-def prepare_element(element):
-    """ Convert OSM element to JSON """
-    if element.tag == "node" or element.tag == "way" or element.tag == "relation":
-        #Common data convertion
-        node = {}
-        node['id'] = int(element.attrib['id'])
-        node['type'] = element.tag
-        node['created'] = get_creation_information(element)
-        #Specific data convertion
-        if element.tag == "node":
-            return prepare_node_element(element, node)
-        elif element.tag == "way":
-            return prepare_way_element(element, node)
-        elif element.tag == "relation":
-            return prepare_relation_element(element, node)
-    else:
-        return None
+CREATED_TAGS = ["version", "changeset", "timestamp", "user", "uid"]
+KEYS_TO_CONVERT = ["amenity", "name", "shop", "leisure", "cuisine", "highway", "area", "type"]
+JSON_DATA = []
 
 def convert_address_tag_element(tag_element, address_node):
     """ Convert the address data from OSM file to JSON structure """
@@ -52,16 +37,49 @@ def convert_address_tag_element(tag_element, address_node):
 def convert_contact_tag_element(tag_element, contact_node):
     """ Convert the contact data from OSM file to JSON structure """
     if audit_contact.is_phone_element(tag_element):
-        pass
+        phone =  audit_contact.clean_phone_number(tag_element.attrib['v'])
+        if phone:
+            contact_node["phone"] = phone
     if audit_contact.is_email_element(tag_element):
         pass
     if audit_contact.is_site_element(tag_element):
         pass
     return contact_node
 
-def prepare_node_element(element, node):
-    """ Convert 'node' elements to JSON format """
-    node['position'] = [float(element.attrib['lat']), float(element.attrib['lon'])]
+def convert_way_node_refs(element):
+    """ 
+    Convert node refences (nd elements) for 'way' elements 
+    """
+    node_refs = []
+    for node_ref in element.iter("nd"):
+        node_refs.append(int(node_ref.attrib['ref']))
+    return node_refs
+
+def convert_member_element(element):
+    """ 
+    Convert members elements from 'relation' elements to JSON format
+    """
+    member_list = {}
+    for member in element.iter("member"):
+        if member.attrib['type'] == "way":
+            if 'ways' not in member_list:
+                member_list['ways'] = []
+            member_list['ways'].append(int(member.attrib['ref']))
+        if member.attrib['type'] == "node":
+            if 'nodes' not in member_list:
+                member_list['nodes'] = []
+            member_list['nodes'].append(int(member.attrib['ref']))
+    return member_list
+
+def get_creation_information(element):
+    """ Extract creation information from OSM element """
+    created = {}
+    for tag in CREATED_TAGS:
+        if tag in element.attrib:
+            created[tag] = element.attrib[tag]
+    return created
+
+def convert_tag_element(element, node):
     for tag_element in element.iter("tag"):
         #Convert address data
         if tag_element.attrib['k'] in audit_address.valid_address_tag:
@@ -76,57 +94,69 @@ def prepare_node_element(element, node):
         #Convert other tags
         if tag_element.attrib['k'] in KEYS_TO_CONVERT:
             node[tag_element.attrib['k']] = tag_element.attrib['v']
-
+    
     return node
+    
 
-def prepare_way_element(element, node):
-    """ Convert 'way' elements to JSON format """
-    for node_ref in element.iter("nd"):
-        if 'node_refs' not in node:
-            node['node_refs'] = []
-        node['node_refs'].append(node_ref.attrib['ref'])
-    return node
+def convert_element(element):
+    """ Convert OSM element to JSON """
+    if element.tag == "node" or element.tag == "way" or element.tag == "relation":
+        #Common data convertion
+        node = {}
+        node['id'] = int(element.attrib['id'])
+        node['created'] = get_creation_information(element)
 
-def prepare_relation_element(element, node):
-    """ Convert 'relation' elements to JSON format """
-    for member in element.iter("member"):
-        if 'members' not in node:
-            node['members'] = {}
-        if member.attrib['type'] == "way":
-            if 'ways' not in node['members']:
-                node['members']['ways'] = []
-            node['members']['ways'].append(member.attrib['ref'])
-        if member.attrib['type'] == "node":
-            if 'nodes' not in node['members']:
-                node['members']['nodes'] = []
-            node['members']['nodes'].append(member.attrib['ref'])
-    return node
+        #Get position data for node type elements
+        if element.tag == "node":
+            node['position'] = [float(element.attrib['lat']), float(element.attrib['lon'])]
 
-def get_creation_information(element):
-    """ Extract creation information from OSM element """
-    created = {}
-    for tag in CREATED_TAGS:
-        if tag in element.attrib:
-            created[tag] = element.attrib[tag]
-    return created
+        #Convert tag elements
+        node = convert_tag_element(element, node)
+
+        #Convert nd elements from a way
+        if element.tag == "way":
+            node_refs = convert_way_node_refs(element)
+            if len(node_refs) > 0:
+                node["node_refs"] = node_refs
+
+        elif element.tag == "relation":
+            members =  convert_member_element(element)
+            if len(members) > 0:
+                node['members'] = members
+
+        return node
+
+    else:
+        return None
+
+def record_document(elem_type, elem, pretty):
+    if elem_type == "node":
+        out = JSON_NODE_OUT
+    elif elem_type == "way":
+        out = JSON_WAY_OUT
+    elif elem_type == "relation":
+        out = JSON_RELATION_OUT
+
+    if pretty:
+        out.write(json.dumps(elem, indent=2)+"\n")
+    else:
+        out.write(json.dumps(elem) + "\n")
 
 def convert_file(file_in, pretty=False):
     """ Convert an OSM file to a JSON format """
-    file_out = "{0}_{1}.json".format(file_in, time.time())
-    with codecs.open(file_out, "w") as out:
-        for _, element in ET.iterparse(file_in, events=("start",)):
-            elem = prepare_element(element)
-            if elem:
-                JSON_DATA.append(elem)
-                if pretty:
-                    out.write(json.dumps(elem, indent=2)+"\n")
-                else:
-                    out.write(json.dumps(elem) + "\n")
+    #file_out = "{0}_{1}.json".format(file_in, time.time())
+    #with codecs.open(file_out, "w") as out:
+    osm_file_in = codecs.open(file_in, 'r', 'utf-8')
+    for _, element in ET.iterparse(osm_file_in, events=("start",)):
+        elem = convert_element(element)
+        if elem:
+            JSON_DATA.append(elem)
+            record_document(element.tag, elem, pretty)
 
 def main():
     """ Main function """
     convert_file(OSM_FILE, True)
-    pprint.pprint(JSON_DATA)
+    #pprint.pprint(JSON_DATA)
 
 if __name__ == "__main__":
     main()
