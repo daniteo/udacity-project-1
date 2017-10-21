9456aae6c4836b15f84a1e25727beec333d93d15
Daniel Teobaldo <contato@danielteobaldo.com.br>
2017-10-21 10:40:42 -0200

    Comentários, alteração de nomes de constantes e correcáo

--- audit_address.py
+++ audit_address.py
@@ -1,35 +1,41 @@
+"""
+Audit addrees contact from OSM file
+"""
 import xml.etree.cElementTree as ET
 import re
-from collections import defaultdict
 
 OSM_FILE = "bh_map.osm"
 
+#Address tags to be convert from OSM file
 VALID_ADDRESS_TAG = ["addr:street",
                      "addr:city",
                      "addr:postcode",
+                     "postal_code",
                      "addr:housenumber",
                      "addr:housename",
                      "addr:suburb"]
 
 #Regex used to validate address data
-STREET_TYPE_RE = re.compile(r'^\S+\.?\b', re.IGNORECASE)
+STREET_TYPE_RE = re.compile(r'^[A-ZÀ-Ú]+((\.?\b)|\.)', re.IGNORECASE)
 ZIPCODE_RE = re.compile(r'^3[0-9]{4}\-[0-9]{3}')
 ADDRESS_NUMBER_RE = re.compile(r'^[0-9]+')
 ADDRESS_NUMBER_WITH_NAME_RE = re.compile(r'^([0-9]+) ?[, \-/]? ?([A-Z0-9 º]+)', re.IGNORECASE)
 
-invalid_street_type_list = {}
-suburb_list = []
-city_list = []
-invalid_address_number_list = []
-invalid_zipcode_list = []
-
-address_tags = set()
+#Data structure used to audit OSM data
+SUBURB_NAME_LIST = []
+CITY_NAME_LIST = []
+INVALID_STREET_TYPE_LIST = {}
+INVALID_ADDRESS_NUMBER_LIST = []
+INVALID_ZIPCODE_LIST = []
 
 #Street Audit Structure
+#Street type expected in structure
+VALID_STREET_TYPE = ["Alameda", "Avenida", "Praça", "Rodovia", "Rua", "Anel", "Estrada"]
 
-valid_street_type = ["Alameda", "Avenida", "Praça", "Rodovia", "Rua"]
-
-street_type_mapping_replace = {
+#Street type that should be replaced from OSM to JSON file
+#in order to keep consistency between data. This mapping 
+#deal with names typed wrong or abbreviations
+STREET_TYPE_MAPPING_REPLACE = {
     "Av" : "Avenida",
     "Avendia" : "Avenida",
     "Anél" : "Anel",
@@ -38,7 +44,10 @@ street_type_mapping_replace = {
     "Alamedas": "Alameda"
 }
 
-street_type_mapping_add = {
+#Street type that should be added to the street name from OSM to JSON file
+#Those street name were analyzed in an iterative waym so I can check the 
+#street names to identified their types since its wasn't inserted at the name.
+STREET_TYPE_MAPPING_ADD = {
     "Br" : "Rodovia",
     "Br-381" : "Rodovia",
     "Dos": "Rua",
@@ -46,53 +55,82 @@ street_type_mapping_add = {
     "Paraíba": "Rua",
     "Montes": "Rua",
     "Contorno": "Avenida",
-    "Pium-i": "Rua",
+    "Pium": "Rua",
     "Riachuelo": "Rua",
     "São" : "Rua"
 }
 
-#City Audit Structure
-region_city_name = ["Belo Horizonte", "Contagem", "Nova Lima", "Ribeirão das Neves", "Santa Luzia", "Sabará"]
+#City Audit Structure:
+#City names expected in Belo Horizonte area, aka "Grande Belo Horizonte"
+CITY_NAME_IN_BELOHORIZONTE_AREA = ["Belo Horizonte",
+                                   "Contagem",
+                                   "Nova Lima",
+                                   "Ribeirão das Neves",
+                                   "Santa Luzia",
+                                   "Sabará"]
 
-city_name_mapping = {
+#City name that should be replaced from OSM to JSON file
+#in order to keep consistency between data. This mapping 
+#deal with names typed wrong or abbreviations
+CITY_NAME_MAPPING = {
     "bh" : "Belo Horizonte",
-    "Belo horizonte" : "Belo Horizonte",
     "Belo Horizonte/MG" : "Belo Horizonte",
-    "belo horizonte" : "Belo Horizonte",
-    "BELO hORIZONTE" : "Belo Horizonte",
     "Belo Horizonte - MG" : "Belo Horizonte",
-    "belo Horizonte" : "Belo Horizonte",
-    "Belo Horizonte MG Brazil" : "Belo Horizonte",
-    "CONTAGEM" : "Contagem",
-    "SANTA LUZIA" : "Santa Luzia",
-    "Santa luzia" : "Santa Luzia"
+    "Belo Horizonte MG Brazil" : "Belo Horizonte"
+}
+
+#Suburb name that should be replaced from OSM to JSON file
+#in order to keep consistency between data. This mapping 
+#deal with names typed wrong 
+SUBURB_NAME_MAPPING = {
+    "Goiania" : "Goiânia",
+    "Padre Eustaquio" : "Padre Eustáquio"
 }
 
 def process_steet_type_and_name(street_name):
     """
+    Extract street type from street name and verify data,
+    correcting wrong data
     """
+    street_name = street_name.title()
     street_type = ""
     match = STREET_TYPE_RE.search(street_name)
     if match:
-        street_type = match.group().lower().capitalize()
-        if street_type in street_type_mapping_replace:
-            street_name = street_name.replace(street_type, street_type_mapping_replace[street_type])
-            street_type = street_type_mapping_replace[street_type]
-        elif street_type in street_type_mapping_add:
-            street_type = street_type_mapping_add[street_type]
+        street_type = match.group()
+        if street_type in STREET_TYPE_MAPPING_REPLACE:
+            street_name = street_name.replace(street_type, STREET_TYPE_MAPPING_REPLACE[street_type])
+            street_type = STREET_TYPE_MAPPING_REPLACE[street_type]
+        elif street_type in STREET_TYPE_MAPPING_ADD:
+            street_type = STREET_TYPE_MAPPING_ADD[street_type]
             street_name = street_type+" "+street_name
     return street_type, street_name
 
-def audit_steet_type(street_name):
+def audit_steet_type_and_name(street_name):
+    """
+    Audit street type and name, checking for consitency and incorrect data
+    """
     street_type, street_name = process_steet_type_and_name(street_name)
-    if street_type not in valid_street_type:
-        if street_type not in invalid_street_type_list:
-            invalid_street_type_list[street_type] = []
-        invalid_street_type_list[street_type].append(street_name)
+    if street_type not in VALID_STREET_TYPE:
+        if street_type not in INVALID_STREET_TYPE_LIST:
+            INVALID_STREET_TYPE_LIST[street_type] = []
+        INVALID_STREET_TYPE_LIST[street_type].append(street_name)
+
+def process_suburb_name(suburb):
+    """
+    Check and cleaning suburb name by formating ans mapping name typed
+    in differnt ways to keep data consistency
+    """
+    if suburb in SUBURB_NAME_MAPPING:
+        suburb = SUBURB_NAME_MAPPING[suburb]
+    return suburb.title()
 
 def audit_suburb_name(suburb):
-    if suburb not in suburb_list:
-        suburb_list.append(suburb)
+    """
+    Audit suburb name, logging in SUBURB_NAME_LIST strutcture after cleaning
+    """
+    suburb = process_suburb_name(suburb)
+    if suburb.title() not in SUBURB_NAME_LIST:
+        SUBURB_NAME_LIST.append(suburb.title())
 
 #Address number audit
 def clean_address_number(number):
@@ -116,22 +154,32 @@ def audit_address_number(number):
     """
     Audit address number format
     """
-    addr_number = clean_address_number(number)
+    addr_number, _ = clean_address_number(number)
     if not addr_number:
-        invalid_address_number_list.append(number)
+        INVALID_ADDRESS_NUMBER_LIST.append(number)
 
 def city_name_cleaning(city):
-    if city in city_name_mapping:
-        city = city_name_mapping[city]
-    if city in region_city_name:
+    """
+    Check and cleaning suburb name by formating ans mapping name typed
+    in differnt ways to keep data consistency
+    """
+    #Format city name captilizing each word from the name
+    city = city.title()
+    #Mapping city name
+    if city in CITY_NAME_MAPPING:
+        city = CITY_NAME_MAPPING[city]
+    if city in CITY_NAME_IN_BELOHORIZONTE_AREA:
         return city
     else:
         return None
 
-def audit_city_name(city): 
-    city = city_name_cleaning(city)   
-    if city and city not in city_list:
-        city_list.append(city)
+def audit_city_name(city):
+    """
+    Audit name city, logging in CITY_NAME_LIST strutcture after cleaning
+    """
+    city = city_name_cleaning(city)
+    if city and city not in CITY_NAME_LIST:
+        CITY_NAME_LIST.append(city)
 
 def clean_zipcode(zipcode):
     """
@@ -156,7 +204,7 @@ def clean_zipcode(zipcode):
 def audit_zipcode(zipcode):
     zipcode_cleaned = clean_zipcode(zipcode)
     if not zipcode_cleaned:
-        invalid_zipcode_list.append(zipcode)
+        INVALID_ZIPCODE_LIST.append(zipcode)
 
 def is_address_element(element):
     """ Check if the element belongs to a address tag """
@@ -184,10 +232,8 @@ def is_zipcode_element(element):
 def audit_address(filename):
     """ Check the address data from OSM file """
     for _, element in ET.iterparse(filename, events=("start",)):
-        if element.tag == "tag" and element.attrib['k'].startswith("addr:"):
-            address_tags.add(element.attrib['k'])
         if is_street_element(element):
-            audit_steet_type(element.attrib['v'])
+            audit_steet_type_and_name(element.attrib['v'])
         elif is_suburb_element(element):
             audit_suburb_name(element.attrib['v'])
         elif is_address_number_element(element):
@@ -198,16 +244,16 @@ def audit_address(filename):
             audit_city_name(element.attrib['v'])
 
 def main():
-    """ 
+    """
     Main function
     Used to audit address data, checking which verification/changing its necessary to be done.
     """
     audit_address(OSM_FILE)
-    print("\nEstrutura:\n {0}\n".format(address_tags))
-    print(invalid_street_type_list)
-    print("\nInvalid zipcodes:\n {0}\n".format(invalid_zipcode_list))
-    print("\nInvalid address numbers:\n {0}\n".format(invalid_address_number_list))
-    print("\nCity list:\n {0}\n".format(city_list))
+    print("\nSuburb names:\n {0}\n".format(sorted(SUBURB_NAME_LIST)))
+    print("\nCity names:\n {0}\n".format(CITY_NAME_LIST))
+    print("\nInvalid street types:\n {0}\n".format(INVALID_STREET_TYPE_LIST))
+    print("\nInvalid zipcodes:\n {0}\n".format(INVALID_ZIPCODE_LIST))
+    print("\nInvalid address numbers:\n {0}\n".format(INVALID_ADDRESS_NUMBER_LIST))
 
 if __name__ == "__main__":
     main()
